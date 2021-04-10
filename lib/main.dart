7426b596032391c3ad0f6a8ce7bdbc3a90712af1
aqshaltata <aqshaltata.matt@gmail.com>
2021-04-10 22:29:24 +0800

    Versi 1 Project Flutter sendiri

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(LatihanFlutter());
+}
+class LatihanFlutter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Latihan Flutter Nih")),
+        body: Center(child: Text("Hello World")),
+      ),
+    );
+  }
+}
